e11c9b56c3734dc3b94928a494d0d3dc3c0880c1
Zach Miller <xhmf1423@gmail.com>
2023-12-23 17:37:28 -0500

    MVP for Rust -> UI Comms (#4)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod mdt;
-use mdt::cmds::{greet};
+use mdt::cmds::{get_nodes};
+use specta::collect_types;
+use tauri_specta::{ts};
 
 fn main() {
+    let ts_binds_output_path = format!("{top_dir}/../ui/bindings/bindings.ts", top_dir=env!("CARGO_MANIFEST_DIR"));
+    ts::export(collect_types![get_nodes], ts_binds_output_path).unwrap();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_nodes])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
