280e4cc4c19812187cdb44d0cbe0fe695d8c1b6c
Zach Miller <xhmf1423@gmail.com>
2024-04-01 21:38:31 -0400

    Convert Canvas from FC to Component for Renderer DFS (#14)

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,20 +1,19 @@
 import "./App.css";
 import {Canvas} from "./canvas/Canvas";
 import {ErrorDisplay} from "./modals/ErrorDisplay";
-import {ErrorStore} from "./stores/ErrorStore";
-import {CanvasStore} from "./stores/CanvasStore";
+import {errorStore} from "./stores/ErrorStore";
+import {canvasStore} from "./stores/CanvasStore";
 import {TopToolbar, LeftToolbar} from "./toolbars/Toolbars";
 
 function App() {
-  const errorStore = new ErrorStore();
-  const canvasStore = new CanvasStore();
-
+  // Note on stores - Functional Components should take stores via props for traceability - but could not get this to
+  //  work right with regular Components. So, pass the same stores Components (like Canvas) access via props to FCs.
   return (
     <div id="app">
       <TopToolbar/>
       <div id="side-by-side">
         <LeftToolbar canvasStore={canvasStore}/>
-        <Canvas errorStore={errorStore} canvasStore={canvasStore}/>
+        <Canvas/>
       </div>
       <ErrorDisplay errorStore={errorStore}/>
     </div>
